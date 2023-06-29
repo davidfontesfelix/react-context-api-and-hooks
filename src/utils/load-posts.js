8e0a8c0e259fd2fe44343dbec37ba2a34b26c94a
davidfontesfelix <davidfontes303@gmail.com>
2023-06-28 23:51:00 -0300

    initial

---
+++ src/utils/load-posts.js
@@ -0,0 +1,15 @@
+export async function loadPosts() {
+  const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+  const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+  const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+  const photosJson = await photos.json();
+  const postsJson = await posts.json();
+
+  const postsAndPhotos = postsJson.map((post, index) => {
+    return { ...post, cover: photosJson[index].url };
+  });
+
+  return postsAndPhotos;
+}
