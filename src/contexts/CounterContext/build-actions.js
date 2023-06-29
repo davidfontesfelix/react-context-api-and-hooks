9516c4f0bd32943736dbe84c664797ae5987d7b3
davidfontesfelix <davidfontes303@gmail.com>
2023-06-29 14:40:15 -0300

    end.

---
+++ src/contexts/CounterContext/build-actions.js
@@ -0,0 +1,35 @@
+import * as actionsTypes from './action-types';
+
+export const buildActions = (dispatch) => {
+  return {
+    increase: () => dispatch({ type: actionsTypes.INCREASE }),
+    decrease: () => dispatch({ type: actionsTypes.DECREASE }),
+    reset: () => dispatch({ type: actionsTypes.RESET }),
+    setCounter: (payload) =>
+      dispatch({ type: actionsTypes.SET_COUNTER, payload }),
+    asyncIncrease: () => asyncIncreaseFn(dispatch),
+    asyncError: () => asyncErrorFn(dispatch),
+  };
+};
+
+const asyncIncreaseFn = async (dispatch) => {
+  dispatch({ type: actionsTypes.ASYNC_INCREASE_START });
+
+  return await new Promise((r) => {
+    setTimeout(() => {
+      dispatch({ type: actionsTypes.ASYNC_INCREASE_END });
+      r('RESOLVED!');
+    }, 2000);
+  });
+};
+
+const asyncErrorFn = async (dispatch) => {
+  dispatch({ type: actionsTypes.ASYNC_INCREASE_ERROR });
+
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      dispatch({ type: actionsTypes.ASYNC_INCREASE_ERROR });
+      reject(new Error('Deu ruim'));
+    }, 2000);
+  });
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
+import { useCounterContext } from '../../contexts/CounterContext';
+import { Button } from '../../components/Button';
+import { Heading } from '../../components/Heading';
+
 export default function Home() {
+  const [state, actions] = useCounterContext();
+
+  const handleError = () => {
+    actions
+      .asyncError()
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e.name, ':', e.message));
+  };
+
   return (
     <div>
-      <h1>oi</h1>
+      <Heading />
+
+      <div>
+        <Button onButtonClick={actions.increase}>increase</Button>
+        <Button onButtonClick={actions.decrease}>decrease</Button>
+        <Button onButtonClick={actions.reset}>reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>
+          set 10
+        </Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>
+          set 100
+        </Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          async increase
+        </Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          async error
+        </Button>
+      </div>
     </div>
   );
 }
